18eb22511a76f498eb3b130ab96bb5e85640baa9
ManikTzyyy <manikyogantara1@gmail.com>
2025-06-29 15:41:42 +0800

    front end auth page

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,12 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/login', function () {
+    return view('authPages/login');
+});
+
+Route::get('/register', function () {
+    return view('authPages/register');
+});
+
 Volt::route('/counter', 'counter');
